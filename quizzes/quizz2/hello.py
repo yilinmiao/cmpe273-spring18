3dffab49acb70f6ce14dc1d9be59df5209109b3d
yilinmiao <yi.matheson@gmail.com>
2018-02-24 17:14:44 -0800

    Quiz2

--- quizzes/quizz2/hello.py
+++ quizzes/quizz2/hello.py
@@ -7,7 +7,21 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route('/users', methods=['POST'])
-def new_users():
+map = {}
+user_id = '1'
+@app.route('/users', methods = ['POST'])
+def user():
+    """modify/update the information for <user_id>"""
+
     name = request.form["name"]
-    return "Hello {}!".format(name)
+    map[user_id] = name
+    return "POST, id = {}, name = {}\n".format(user_id, name)
+
+@app.route('/users/<user_id>', methods = ['GET','DELETE'])
+def getname(user_id):
+    #user_id = request.get["user_id"]
+    if(request.method == 'GET'):
+        return "GET, id = {}, name = {}\n".format(user_id, map[user_id])
+    else:
+        map[user_id] = ''
+        return "No Content"
