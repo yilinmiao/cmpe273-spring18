98864841784405e93d76aa9d14b2bb5f7f34e7d2
admin <admin@USCS-Mac097.local>
2018-03-08 23:18:50 -0800

    .

--- lab3/client.py
+++ lab3/client.py
@@ -1,12 +1,10 @@
 import zmq
 import sys
-from zmq.eventloop import ioloop, zmqstream
 from multiprocessing import Process
 
 
 def req_sender(user):
     context = zmq.Context()
-    # push own messages to server
     sock_push = context.socket(zmq.PUSH)
     sock_push.connect("tcp://127.0.0.1:5678")
     while True:
@@ -19,24 +17,14 @@ def req_sender(user):
 
 
 def sub_receiver(user):
-    # Tornado Event Loop
-    ioloop.install()
     context = zmq.Context()
-    # subscribe other messages from server
     sock_sub = context.socket(zmq.SUB)
-    # no filter
     sock_sub.setsockopt_string(zmq.SUBSCRIBE, '')
-    sock_sub.connect("tcp://127.0.0.1:5679")
-    # register a print for when a message is ready to receive
-    stream_sub = zmqstream.ZMQStream(sock_sub)
-
-    def print_message(msg):
-        msg = b''.join(msg).decode('ascii')
+    sock_sub.connect("tcp://127.0.0.1:5600")
+    while True:
+        msg = sock_sub.recv_string()      
         if msg.find(user) == -1:
-            print(msg)
-
-    stream_sub.on_recv(print_message)
-    ioloop.IOLoop.instance().start()
+            print("\n" + msg + "\n[%s] > " % user, end='')
 
 
 if __name__ == "__main__":
--- lab3/server.py
+++ lab3/server.py
@@ -1,26 +1,16 @@
 import zmq
-import time
 
 
 def start():
     context = zmq.Context()
-    # Pull messages from each client
     sock_pull = context.socket(zmq.PULL)
-    # publish messages to all clients
     sock_pub = context.socket(zmq.PUB)
     sock_pull.bind("tcp://127.0.0.1:5678")
-    sock_pub.bind("tcp://127.0.0.1:5679")
-    # ZMQ Poller
-    poller = zmq.Poller()
-    poller.register(sock_pull, zmq.POLLIN)
+    sock_pub.bind("tcp://127.0.0.1:5600")
     while True:
-        socks = dict(poller.poll())
-        # poll the sockets to check if we have messages to recv and publish it
-        if sock_pull in socks and socks[sock_pull] == zmq.POLLIN:
-            msg = sock_pull.recv_string()
-            sock_pub.send_string(msg)
-            print(msg)
-        time.sleep(0.1)
+        msg = sock_pull.recv_string()
+        sock_pub.send_string(msg)
+        print(msg)
 
 
 if __name__ == "__main__":
